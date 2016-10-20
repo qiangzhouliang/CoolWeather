25bcf127970c5cab59fe9add5c79d2863415da9a
Qzl <2538096489@qq.com>
2016-10-20 19:40:59 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/qzl/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/qzl/coolweather/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.qzl.coolweather.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -57,8 +58,8 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
             //没有县级代号时就直接显示本地天气
             showWeather();
         }
-        //switchCity.setOnClickListener(this);
-        //refreshWeather.setOnClickListener(this);
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
     }
 
     /**
@@ -145,11 +146,30 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
         currentDateText = (TextView) findViewById(R.id.current_date);
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
 
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.switch_city:
+                Intent intent = new Intent(this,ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity",true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("同步中...");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = prefs.getString("weather_code","");
+                if (!TextUtils.isEmpty(weatherCode)){
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
